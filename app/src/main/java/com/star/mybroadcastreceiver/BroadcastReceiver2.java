9a553fd5612d0d7f0f8e1cdbf7553f329c2a56ae
star <s0913940588@gmail.com>
2017-11-21 09:49:30 +0800

    1.使用Manifest註冊 2.使用Java註冊(動態註冊，必須APP保持打開才有作用)

--- app/src/main/java/com/star/mybroadcastreceiver/BroadcastReceiver2.java
+++ app/src/main/java/com/star/mybroadcastreceiver/BroadcastReceiver2.java
@@ -3,6 +3,7 @@ package com.star.mybroadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by Star on 2017/11/21.
@@ -11,6 +12,7 @@ import android.content.Intent;
 public class BroadcastReceiver2 extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        String event = intent.getAction();
+        Toast.makeText(context , "BR2 : " + event ,Toast.LENGTH_SHORT ).show();
     }
 }
--- app/src/main/java/com/star/mybroadcastreceiver/MainActivity.java
+++ app/src/main/java/com/star/mybroadcastreceiver/MainActivity.java
@@ -1,5 +1,7 @@
 package com.star.mybroadcastreceiver;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerReceiver();
+    }
+
+    void registerReceiver()
+    {
+        BroadcastReceiver2 br2 = new BroadcastReceiver2();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_ADDED);
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addDataScheme("package");
+        super.registerReceiver(br2 ,filter); //跟自訂的方法同名，so 加 super繼承
     }
 }
