1d8ed24d1ec6dd0885010dc9d310da74a2a07a94
star <s0913940588@gmail.com>
2017-11-21 09:27:59 +0800

    1.使用Manifest註冊

--- app/src/main/java/com/star/mybroadcastreceiver/BroadcastReceiver2.java
+++ app/src/main/java/com/star/mybroadcastreceiver/BroadcastReceiver2.java
@@ -3,17 +3,14 @@ package com.star.mybroadcastreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 /**
  * Created by Star on 2017/11/21.
  */
 
-public class BroadcastReceiver1 extends BroadcastReceiver {
-    @Override  // 收廣播
+public class BroadcastReceiver2 extends BroadcastReceiver {
+    @Override
     public void onReceive(Context context, Intent intent) {
-        String event = intent.getAction();
-        Toast.makeText(context ,event ,Toast.LENGTH_SHORT ).show();
 
     }
 }
