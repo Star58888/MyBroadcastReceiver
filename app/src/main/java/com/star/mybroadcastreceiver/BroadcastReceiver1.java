feef13cc49bed64171fb0e6939f27c7831e17a18
star <s0913940588@gmail.com>
2017-11-21 09:00:36 +0800

    Initial commit

---
+++ app/src/main/java/com/star/mybroadcastreceiver/BroadcastReceiver1.java
@@ -0,0 +1,16 @@
+package com.star.mybroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Star on 2017/11/21.
+ */
+
+public class BroadcastReceiver1 extends BroadcastReceiver {
+    @Override  // 收廣播
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
